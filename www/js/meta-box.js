aa3161d56232a56b420c9bb42fb855f7338266a4
Peter Keane <pkeane@mail.utexas.edu>
2010-05-07 01:16:21 -0500

    initial

---
+++ www/js/meta-box.js
@@ -0,0 +1,34 @@
+var MetaBox = {};
+
+$(document).ready(function() {
+	MetaBox.initDelete('itemsList');
+//	MetaBox.initToggle('itemlist');
+});
+
+MetaBox.initToggle = function(id) {
+	$('#'+id).find('a[class="toggle"]').click(function() {
+		var id = $(this).attr('id');
+		var tar = id.replace('toggle','target');
+		$('#'+tar).toggle();
+		return false;
+	});	
+};
+
+MetaBox.initDelete = function(id) {
+	$('#'+id).find("a[class='delete']").click(function() {
+		if (confirm('are you sure?')) {
+			var del_o = {
+				'url': $(this).attr('href'),
+				'type':'DELETE',
+				'success': function() {
+					location.reload();
+				},
+				'error': function() {
+					alert('sorry, cannot delete');
+				}
+			};
+			$.ajax(del_o);
+		}
+		return false;
+	});
+};
